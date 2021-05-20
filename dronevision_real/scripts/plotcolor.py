bf1a64d8a62c41da8beed796f8aa2047cb578f0a
thomas_sabbe <39916843+thomassabbe@users.noreply.github.com>
2021-05-20 20:17:01 +0200

    Update plotcolor.py

--- dronevision_real/scripts/plotcolor.py
+++ dronevision_real/scripts/plotcolor.py
@@ -1,7 +1,13 @@
-from 
+from dronevision_library import plotcolor
+
+##################################################################
+# SAMPLE DEFINITION, TO CALL THE FUNCTION IN THE LIBRARY, BELOW#
+##################################################################
+# For further documentation, please visit https://github.com/thomassabbe/drone-object-detection-using-haar-cascades/blob/main/README.md
 
 # If you wish to plot every color value of pictures inside a folder (put one image to only obtain the HSV plot of one image)
-"Keyword arguments:
+def plotcolor_of_image()
+ "Keyword arguments:
  image -- Image that needs to be plotted.
     color_space -- "RGB" or "HSV"
   "
