1dbb0488effe20235ba135b28e8a089a836238e0
thomas_sabbe <39916843+thomassabbe@users.noreply.github.com>
2021-05-20 20:18:04 +0200

    Update plotcolor.py

--- dronevision_real/scripts/plotcolor.py
+++ dronevision_real/scripts/plotcolor.py
@@ -8,8 +8,14 @@ from dronevision_library import plotcolor
 # If you wish to plot every color value of pictures inside a folder (put one image to only obtain the HSV plot of one image)
 def plotcolor_of_image()
  "Keyword arguments:
- image -- Image that needs to be plotted.
+  #Read the image first, please specify the location:  
+  
+  image -- Image that needs to be plotted.
     color_space -- "RGB" or "HSV"
-  "
-
+    # Please adjust these parameters before running the script:
+    scantime = 0  # Time that the function will scan before returning an output.
+    safezonesize = 0  # DEBUG PARAMETER! Will not be used if you do not wish to debug.
+    desiredcolor = "white"  # Color of object that should be detected. For example "white". Visit README.md for available options.
+    debug = False  # This will enable debugging output, such as text in the runtime environment.
+    videosource = ''  # Determine the video source (video-file). If no input, or the video file could not be opened, camera capture will initialize.
 plotcolor(image, color_space):
