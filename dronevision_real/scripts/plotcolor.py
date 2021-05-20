0e90e2df7cf0e0722f2edfa703e61b2ab2e0d2c9
thomas_sabbe <39916843+thomassabbe@users.noreply.github.com>
2021-05-20 20:16:01 +0200

    Create plotcolor.py

---
+++ dronevision_real/scripts/plotcolor.py
@@ -0,0 +1,9 @@
+from 
+
+# If you wish to plot every color value of pictures inside a folder (put one image to only obtain the HSV plot of one image)
+"Keyword arguments:
+ image -- Image that needs to be plotted.
+    color_space -- "RGB" or "HSV"
+  "
+
+plotcolor(image, color_space):
