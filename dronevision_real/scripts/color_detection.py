75dd17c8e032c52acb4128cb61f0502495062fdb
thomas_sabbe <39916843+thomassabbe@users.noreply.github.com>
2021-05-21 10:40:20 +0200

    Update color_detection.py
    Added 3D HSV/RGB plots

--- dronevision_real/scripts/color_detection.py
+++ dronevision_real/scripts/color_detection.py
@@ -9,9 +9,11 @@ import cv2
 
 def color_detection():
     # Please adjust these parameters before running the script:
-    source_image_path = "../data/training_dataset/blue/blue2.jpg"  # "PATH_TO_IMAGE"  # Fill in the path to the file where your image is stored. Syntax: '../ANYFOLDER/%IMAGE%.png' !!Case sensitive!!
+    source_image_path = ""  # "PATH_TO_IMAGE"  # Fill in the path to the file where your image is stored. Syntax: '../ANYFOLDER/%IMAGE%.png' !!Case sensitive!!
     debugparam = True  # This will enable debugging output, such as text in the runtime environment.
     # Debugging outputs (text) of color will be enabled by default.
     source_image = cv2.imread(source_image_path)
-    color = color_controller(source_image, debugparam)
+    print_test_and_training = True  # Will print a 3D plot of the HSV value of the observed picture inside the clusters of the training dataset.
+    debugcolor = "RGB"  # Choose between HSV or RGB here to display a(nother) plot after color detection. Only active when debugparam = True. Has to be a string ("...")
+    color = color_controller(source_image, debugparam, debugcolor, print_test_and_training)
     print(color)  # Remove this if you don't want the color to be displayed
