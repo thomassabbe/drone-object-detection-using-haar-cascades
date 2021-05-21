a2a4fbe2820b4756daf2fb007548e45e713c814f
thomas_sabbe <39916843+thomassabbe@users.noreply.github.com>
2021-05-21 11:15:44 +0200

    Update plotcolor.py

--- dronevision_real/scripts/plotcolor.py
+++ dronevision_real/scripts/plotcolor.py
@@ -8,8 +8,8 @@ from dronevision_library import plotcolor
 # If you wish to plot every color value in a 3D HSV/RGB plot of a picture
 def plotcolor_of_image()
   #Read the image first, please specify the location:  
-  source_image_path = "../data/training_dataset/blue/blue2.jpg"  # "PATH_TO_IMAGE"  # Fill in the path to the file where your image is stored. Syntax: '../ANYFOLDER/%IMAGE%.png' !!Case sensitive!!
+  source_image_path = ""  # "PATH_TO_IMAGE"  # Fill in the path to the file where your image is stored. Syntax: '../ANYFOLDER/%IMAGE%.png' !!Case sensitive!!
   # Debugging outputs (text) of color will be enabled by default.
   source_image = cv2.imread(source_image_path)
   color_space = "" # Please specify "RGB" or "HSV"
-  plotcolor(source_image, color_space):
+  plotcolor(source_image, color_space)
